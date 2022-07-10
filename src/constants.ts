bf217ee80a00717c8ddee3879cfe38aaef7f40ee
dawidos234 <dawidos234@gmail.com>
2022-07-10 16:00:21 +0200

    remove build files

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const YT_API_KEY = 'AIzaSyBfPGVcy5jdxhRqdojfXOuH1OF2TS71ctk';
 //export const YT_API_URL = 'http://localhost:3000/mock.json';
 export const YT_API_URL = 'https://youtube.googleapis.com/youtube/v3/search';
-export const BACKEND_URL = 'https://youtube.dawidstachowiak.pl:81'
+export const BACKEND_URL = 'https://europe-central2-youtube-player-346010.cloudfunctions.net/youtube-audio-url'
